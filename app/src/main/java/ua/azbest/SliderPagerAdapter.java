9654c7ddae5b90d708fc4cf13408cabd7ee24a48
Yurii-P-Biliai <u.p.bilyay@npu.edu.ua>
2021-01-03 22:37:14 +0200

    first try

---
+++ app/src/main/java/ua/azbest/SliderPagerAdapter.java
@@ -0,0 +1,34 @@
+package ua.azbest;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.List;
+
+public class SliderPagerAdapter extends FragmentStatePagerAdapter {
+
+    private List<Fragment> fragmentList;
+
+    public SliderPagerAdapter(@NonNull FragmentManager fm, List<Fragment> fragmentList) {
+        super(fm);
+        this.fragmentList = fragmentList;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+}
